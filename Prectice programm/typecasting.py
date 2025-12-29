30f67d63c434b06a66ade693fde195c74d714082
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-12-29 19:15:46 +0530

    commit

---
+++ Prectice programm/typecasting.py
@@ -0,0 +1,11 @@
+name = "Dhruv"
+age = 19
+Mnum = 9099432835
+d = "true"
+cgpa = 8.9
+
+print(name, type(name))
+print(age, type(age))
+print(Mnum, type(Mnum))
+print(d, type(d))
+print(cgpa, type(cgpa))
---
+++ Prectice programm/variable.py
@@ -0,0 +1,7 @@
+name = "Dhruv"
+surname = "Makwana"
+age = 19
+
+print(name)
+print(surname)
+print(age)
